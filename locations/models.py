5b3fc7bff8a65ffc433f7e09b8226db6980b6954
Alejandro Reyna <aleejandroreyna@gmail.com>
2022-11-23 23:49:45 -0600

    car-models: Added location model

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,17 @@
+from car_dealer.abstract_models import SoftDeleteModel
 from django.db import models
+
+
+from geography.models import City
+
+
+class Location(SoftDeleteModel):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 
