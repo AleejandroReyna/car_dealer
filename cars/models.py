85558b5569a3abb918d43ee1827d9cab892772fc
Alejandro Reyna <aleejandroreyna@gmail.com>
2022-11-24 00:29:57 -0600

    car-models: Added car model and migration

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,19 @@
+from email.policy import default
+from car_dealer.abstract_models import SoftDeleteModel
 from django.db import models
+import uuid
 
-# Create your models here.
+
+from locations.models import Location
+from brands.models import Brand
+
+
+class Car(SoftDeleteModel):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    codename = models.CharField(max_length=255)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.codename
